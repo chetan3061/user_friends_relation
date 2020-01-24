224c0b24da1b46b5a03c4f241a75e39a0e4f2831
Chetan Puppala <manikanta.ions@gmail.com>
2020-01-24 19:26:25 +0530

    added Person model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    full_name = models.CharField(max_length=255,)
+    friends = models.ManyToManyField('self', blank=True)
+
+    def __str__(self):
+        return self.full_name
+
--- users_friends_relation/settings.py
+++ users_friends_relation/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -75,11 +76,12 @@ WSGI_APPLICATION = 'users_friends_relation.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
+        'NAME': 'user_friend_relation',
+        'HOST': 'localhost',
+    },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
